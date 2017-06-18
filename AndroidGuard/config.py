e672e0fe576a5a637f26127b30d923452e947728
Michael Jabbour <micjabbour@gmail.com>
2017-06-18 18:01:38 +0300

    add proxy configuration in config.py

--- AndroidGuard/config.py
+++ AndroidGuard/config.py
@@ -12,7 +12,8 @@ class AppConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'androidguard.db')
     # TODO: add your firebase cloud messaging API key
     FCM_API_KEY = ''
-    FCM_PROXY_DICT = {
-        "http": "http://127.0.0.1",
-        "https": "http://127.0.0.1",
-    }
+    # uncomment if you want to use a proxy server
+    # FCM_PROXY_DICT = {
+    #     "http": "http://127.0.0.1:8080",
+    #     "https": "http://127.0.0.1:8080",
+    # }
